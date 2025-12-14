fcae2951b1772c3f0be13305357f76d7ca89aeb3
Basanagoud Patil <basanagoudpatil43@gmail.com>
2025-12-14 21:13:04 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "Generative AI Project",
+    version = '0.0.0',
+    author="Basanagoud Patil",
+    author_email="basanagoudpatil246@gmail.com",
+    packages=find_packages(),
+    install_requires = []
+)
